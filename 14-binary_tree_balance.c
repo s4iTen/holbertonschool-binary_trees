2b5f10d4d5ef32fafa6829859b9352c30874899b
s4iTen <ychouk102@gmail.com>
2023-01-11 12:01:15 +0100

    this function gives the balance factor of the binary tree

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,19 +1,34 @@
 #include "binary_trees.h"
 /**
- * binary_tree_balence - this function gives the measures the balance factor of 
- * a binary tree
+ * height - this function count the height of the binary tree
+ * @tree: this is the node
+ * Return: Always the height or 0
+ */
+int height(const binary_tree_t *tree)
+{
+	int r = 0, l = 0;
+
+	if (!tree)
+		return (0);
+	if (!tree->right && !tree->left)
+		return (1);
+	if (tree->right)
+		r = 1 + height(tree->right);
+	if (tree->left)
+		l = 1 + height(tree->left);
+	if (l > r)
+		return (l);
+	return (r);
+}
+/**
+ * binary_tree_balance - this func gives the balance factor of a binary tree
  * @tree: this is the binary tree
  * Return: always the balance or 0 if it fail
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int left = 0, right = 0;
 
 	if (!tree)
 		return (0);
-	if (tree->left)
-		left = 1 + binary_tree_balance(tree->left);
-	if (tree->right)
-		right = 1 + binary_tree_balance(tree->right);
-	return (left - right);
+	return (height(tree->left) - height(tree->right));
 }
