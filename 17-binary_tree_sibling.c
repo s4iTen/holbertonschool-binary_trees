4b1dbd8a8e12699ed7792a2fc0e39fc92a27855b
s4iTen <ychouk102@gmail.com>
2023-01-11 14:45:21 +0100

    this function gives the sibling of the current node in the binary tree

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_sibling - this function gives the sibling of the node
+ * @node: this is the node
+ * Return: Always the node that we're looking for or NULL
+ */
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (!node)
+		return (NULL);
+	if (!node->parent)
+		return (NULL);
+	if (node->parent->left == node)
+		return (node->parent->right);
+	return (node->parent->left);
+}
